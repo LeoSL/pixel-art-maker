a4e7ffed6c5bcc4aaf6b6acfb520c1c9ec858fa0
LeoSL <leosilvalima@gmail.com>
2017-11-26 18:33:49 -0500

    Adds ESlint configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'env': {
+      'browser': true,
+      'es6': true
+  },
+  'extends': 'eslint:recommended',
+  'rules': {
+    'indent': [2, 2],
+    'linebreak-style': [2, 'unix'],
+    'quotes': [2, 'single'],
+    'semi': [2, 'never'],
+    'brace-style': [2, '1tbs'],
+    'array-bracket-spacing': [2, 'never'],
+    'camelcase': [2, {'properties': 'always'}],
+    'keyword-spacing': [2],
+    'eol-last': [2],
+    'no-trailing-spaces': [2],
+    'no-debugger': [0]
+  },
+};
